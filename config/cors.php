4dac8b1c92c6852f8965293e31899734b6d45a68
maaz56 <maaz.ahmed56@gmail.com>
2026-06-25 16:31:30 +0500

    update

--- config/cors.php
+++ config/cors.php
@@ -34,15 +34,16 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
+    'allowed_origins' => ['*'],
 
-    'allowed_origins' => array_values(array_filter([
-        config('app.frontend_url'),
-        'https://jetze.ae',
-        'https://www.jetze.ae',
-        'http://localhost:3000',
-        'http://localhost:5173',
-        'http://127.0.0.1:5173',
-    ])),
+    // 'allowed_origins' => array_values(array_filter([
+    //     config('app.frontend_url'),
+    //     'https://jetze.ae',
+    //     'https://www.jetze.ae',
+    //     'http://localhost:3000',
+    //     'http://localhost:5173',
+    //     'http://127.0.0.1:5173',
+    // ])),
     
 
     'allowed_origins_patterns' => [
